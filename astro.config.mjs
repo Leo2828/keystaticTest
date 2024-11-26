f20e040f4c603b15d8c8928a6ed27483296d8971
Leo2828 <leoleohenrique601@gmail.com>
2024-11-26 14:36:23 -0300

    as

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
   output: 'hybrid',
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
